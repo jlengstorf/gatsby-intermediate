b16794bda2ad0e7d51cd5d9aa5895b4310fb706f
Jason Lengstorf <jason@lengstorf.com>
2019-10-21 01:27:45 -0500

    feat(negronis): add image support

--- sites/negronis/gatsby-config.js
+++ sites/negronis/gatsby-config.js
@@ -1,3 +1,25 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  plugins: ['gatsby-plugin-theme-ui'],
+  plugins: [
+    'gatsby-plugin-theme-ui',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'src/images',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-cloudinary',
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        uploadFolder: 'fem-workshop',
+      },
+    },
+  ],
 };
--- sites/negronis/src/pages/index.js
+++ sites/negronis/src/pages/index.js
@@ -1,11 +1,25 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import Image from 'gatsby-image';
 import Layout from '../components/layout';
 
-const Index = () => (
+export const query = graphql`
+  query {
+    image: file(name: { eq: "negroni" }) {
+      cloudinary: childCloudinaryAsset {
+        fluid(transformations: ["ar_18:9", "c_fill", "g_auto:subject"]) {
+          ...CloudinaryAssetFluid
+        }
+      }
+    }
+  }
+`;
+
+const Index = ({ data }) => (
   <Layout>
     <h1>Negronis: A Love Story</h1>
+    <Image fluid={data.image.cloudinary.fluid} alt="Negroni." />
     <p>
       A Negroni, while simple, is a beautifully complex cocktail with endless
       opportunities to experiment with flavors.

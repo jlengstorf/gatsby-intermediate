25e3531cc9a0ebf4e78aa8d39ae51b1e4088f254
Jason Lengstorf <jason@lengstorf.com>
2019-10-21 14:27:29 -0500

    feat(negronis): add the docs theme for recipes

--- sites/negronis/src/gatsby-plugin-theme-ui/index.js
+++ sites/negronis/src/gatsby-plugin-theme-ui/index.js
@@ -2,6 +2,10 @@ import { roboto as theme } from '@theme-ui/presets';
 
 export default {
   ...theme,
+  colors: {
+    ...theme.colors,
+    muted: '#555',
+  },
   button: {
     hollow: {
       bg: 'background',
@@ -21,6 +25,26 @@ export default {
       },
       ':hover': {
         bg: 'highlight',
+      },
+    },
+    primary: {
+      bg: 'primary',
+      border: theme => `2px solid ${theme.colors.primary}`,
+      borderRadius: 10,
+      color: 'background',
+      display: 'inline-block',
+      fontFamily: 'heading',
+      fontWeight: 'bold',
+      p: 3,
+      textDecoration: 'none',
+      transition: '200ms linear background',
+      ':focus': {
+        bg: 'secondary',
+        outline: theme => `2px solid ${theme.colors.primary}`,
+        outlineOffset: 2,
+      },
+      ':hover': {
+        bg: 'secondary',
       },
     },
   },

f73b33333846b35448bbf9e3fa1417051bd07b3e
Jason Lengstorf <jason@lengstorf.com>
2019-10-20 23:40:19 -0500

    feat(theme-dev): schema customization

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -12,3 +12,66 @@ exports.onPreBootstrap = ({ store }, options) => {
     mkdirp.sync(dir);
   }
 };
+
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(`
+    type DocsPage implements Node @dontInfer {
+      id: ID!
+      title: String!
+      path: String!
+      updated: Date! @dateformat
+      body: String!
+    }
+  `);
+};
+
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+  const { basePath } = withDefaults(options);
+  const parent = getNode(node.parent);
+
+  // Only work on MDX files that were loaded by this theme
+  if (
+    node.internal.type !== 'Mdx' ||
+    parent.sourceInstanceName !== 'gatsby-theme-docs'
+  ) {
+    return;
+  }
+
+  // Treat `index.mdx` like `index.html` (i.e. `docs/` vs. `docs/index`)
+  const pageName = parent.name !== 'index' ? parent.name : '';
+
+  actions.createNode({
+    id: createNodeId(`DocsPage-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'DocsPage',
+      contentDigest: node.internal.contentDigest,
+    },
+  });
+};
+
+exports.createResolvers = ({ createResolvers }) => {
+  createResolvers({
+    DocsPage: {
+      body: {
+        type: 'String!',
+        resolve: (source, args, context, info) => {
+          // Load the resolver for the `Mdx` type’s body field.
+          const type = info.schema.getType('Mdx');
+          const mdxFields = type.getFields();
+          const resolver = mdxFields.body.resolve;
+
+          // Load the `body` content from the parent MDX node and return that.
+          const mdxNode = context.nodeModel.getNodeById({ id: source.parent });
+
+          return resolver(mdxNode, args, context, {
+            fieldName: 'body',
+          });
+        },
+      },
+    },
+  });
+};

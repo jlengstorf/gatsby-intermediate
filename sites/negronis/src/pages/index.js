a911be07262cea03e2d384a808f73dee59557997
Jason Lengstorf <jason@lengstorf.com>
2019-10-21 01:02:42 -0500

    fix(negronis): remove code that shouldn’t be here yet

--- sites/negronis/src/pages/index.js
+++ sites/negronis/src/pages/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Index = () => (
-  <Layout>
+  <>
     <h1>Negronis: A Love Story</h1>
     <p>
       A Negroni, while simple, is a beautifully complex cocktail with endless
       opportunities to experiment with flavors.
     </p>
     <Link to="/history">Learn the History</Link>
-  </Layout>
+  </>
 );
 
 export default Index;

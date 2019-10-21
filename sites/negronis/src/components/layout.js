d9af348576d610cf026e288a6e811288cc1851f1
Jason Lengstorf <jason@lengstorf.com>
2019-10-21 01:12:52 -0500

    feat(negronis): add Theme UI

---
+++ sites/negronis/src/components/layout.js
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { Fragment } from 'react';
+import { jsx } from 'theme-ui';
+import { Global } from '@emotion/core';
+import { Link } from 'gatsby';
+
+const Layout = ({ children }) => (
+  <Fragment>
+    <Global
+      styles={theme => ({
+        '*': {
+          boxSizing: 'border-box',
+        },
+        body: {
+          fontFamily: theme.fonts.body,
+          fontSize: 20,
+          margin: 0,
+        },
+      })}
+    />
+    <header sx={{ bg: 'primary', color: 'background', p: 3 }}>
+      <Link
+        to="/"
+        sx={{
+          color: 'background',
+          fontWeight: 'bold',
+          textDecoration: 'none',
+          ':focus': {
+            outline: theme => `2px solid ${theme.colors.background}`,
+            outlineOffset: 4,
+          },
+        }}
+      >
+        Negroni Appreciation Society
+      </Link>
+    </header>
+    <main sx={{ my: 4, mx: 'auto', maxWidth: 650, width: '90vw' }}>
+      {children}
+    </main>
+  </Fragment>
+);
+
+export default Layout;
--- sites/negronis/src/pages/index.js
+++ sites/negronis/src/pages/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 const Index = () => (
-  <>
+  <Layout>
     <h1>Negronis: A Love Story</h1>
     <p>
       A Negroni, while simple, is a beautifully complex cocktail with endless
       opportunities to experiment with flavors.
     </p>
-    <Link to="/history">Learn the History</Link>
-  </>
+    <Link to="/history" sx={{ variant: 'button.hollow' }}>
+      Learn the History
+    </Link>
+  </Layout>
 );
 
 export default Index;

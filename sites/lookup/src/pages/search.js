5b664accdd63ce4bae13b2de717f7e7a66c6016a
Jason Lengstorf <jason@lengstorf.com>
2019-10-21 14:49:22 -0500

    feat(lookup): set up client-side routing

---
+++ sites/lookup/src/pages/search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Index = () => (
+  <>
+    <h1>Search for Rick & Morty Characters</h1>
+    <p>
+      Trying to remember which Rick you’re talking about? Try out this handy
+      search interface!
+    </p>
+    {/* TODO Add search form */}
+    {/* TODO Add results */}
+  </>
+);
+
+export default Index;

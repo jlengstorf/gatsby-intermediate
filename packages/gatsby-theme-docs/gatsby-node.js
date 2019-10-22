a94991d66a9ab4adf308d38ddaeaefe457b69a8e
faisa1khan <faisalkhan5464@gmial.com>
2019-10-23 03:39:07 +0530

    feat(theme-dev): add onprebootstrap

---
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const withDefaults = require('./utils/default-options');
+
+exports.onPreBootstrap = ({ store }, options) => {
+  const { program } = store.getState();
+  const { contentPath } = withDefaults(options);
+  const dir = path.join(program.directory, contentPath);
+  if (!fs.existsSync(dir)) {
+    mkdirp.sync(dir);
+  }
+};
---
+++ packages/gatsby-theme-docs/utils/default-options.js
@@ -0,0 +1,5 @@
+module.exports = ({
+  basePath = '/',
+  contentPath = 'docs',
+  useExternalMDX = false,
+}) => ({ basePath, contentPath, useExternalMDX });

133337ec10bf9110c3e0a8f9119f6e537c3081b2
Dan Schmidt <daniel.adam.schmidt@gmail.com>
2020-03-29 05:25:57 -0500

    feat(theme-dev): add preBootStrap setup

---
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const withDefaults = require('./utils/default-options');
+
+exports.onPreBootstrap = ({ store }, options) => {
+  const { program } = store.getState();
+  const { contentPath } = withDefaults(options);
+  const dir = path.join(program.directory, contentPath);
+
+  if (!fs.existsSync(dir)) {
+    mkdirp.sync(dir);
+  }
+};

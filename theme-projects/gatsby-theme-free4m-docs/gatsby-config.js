258b8c4e49529aa2fd2618cfc79f8a026be10520
David Pineda <david.pineda@deparadise.com>
2020-04-06 14:53:42 -0700

    Theme MDX configuration

--- theme-projects/gatsby-theme-free4m-docs/gatsby-config.js
+++ theme-projects/gatsby-theme-free4m-docs/gatsby-config.js
@@ -1,3 +1,25 @@
-module.exorts = {
-	plugins: [],
+const withDefaults = require('./utils/default-options');
+
+module.exorts = (options) => {
+	const {contentPath, useExternalMDX} = withDefaults(options);
+
+	return {
+		plugins: [
+			{
+				resolve: 'gatsby-source-filesystem',
+				options: {
+					name: 'gatsby-theme-free4m-docs',
+					path: contentPath,
+				},
+			},
+			!userExternalMDX && {
+				resolve: 'gatsby-plugin-mdx',
+				options: {
+					defaultLayouts: {
+						default: require.resolve('./src/components/layout.js'),
+					},
+				},
+			},
+		],
+	};
 };

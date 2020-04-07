fa446663d3ab93ec9044cc2322179099070192e1
David Pineda <david.pineda@deparadise.com>
2020-04-07 00:44:38 -0700

    Create sample custom data type for document nodes/files

--- theme-projects/gatsby-theme-free4m-docs/gatsby-node.js
+++ theme-projects/gatsby-theme-free4m-docs/gatsby-node.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const withDefaults = require('./utils/default-options');
 
+// CREATE DOCUMETNS DIRECTORY
 exports.onPreBootstrap = ({store}, options) => {
-	// CREATE DOCUMETNS DIRECTORY
 	const {program} = store.getState();
 	const {contentPath} = withDefaults(options);
 	const dir = path.join(program.directory, contentPath);
@@ -12,4 +12,17 @@ exports.onPreBootstrap = ({store}, options) => {
 	if (!fs.existsSync(dir)) {
 		mkdirp.sync(dir);
 	}
+};
+
+// SAMPLE CUSTOM DATA TYPE FOR DOCUMENTS
+exports.createSchemaCustomization = ({actions}) => {
+	actions.createTypes(`
+	type DocsPage implements Node @dontInfer {
+		id: ID!
+		title: String!
+		path: String!
+		updated: Date! @dateformat
+		body: String!
+	}
+ `);
 };

9f94ba4f9b1f4767b9c44f16fc6b1fc770f878d8
David Pineda <david.pineda@deparadise.com>
2020-04-10 07:25:46 -0700

    Swap React for theme-ui/jsx pragma

--- theme-projects/gatsby-theme-free4m-docs/src/components/theme-layout.js
+++ theme-projects/gatsby-theme-free4m-docs/src/components/theme-layout.js
@@ -1,12 +1,14 @@
+/** @jsx jsx **/
 // 3.) >> Renders common theme components and scaffold >>
 
-import React from 'react';
+import {Fragment} from 'react';
+import {jsx} from 'theme-ui';
 
 const ThemeLayout = ({children}) => (
-	<>
+	<Fragment>
 		<header>GATSBY THEME FREE4M DOCS</header>
 		<main>{children}</main>
-	</>
+	</Fragment>
 );
 
 export default ThemeLayout;

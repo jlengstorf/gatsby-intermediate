7f5c78cef06c5f77b02c8402668b188706bb0489
David Pineda <david.pineda@deparadise.com>
2020-04-11 12:08:34 -0700

    START: CodeSample component

---
+++ theme-projects/gatsby-theme-free4m-docs/src/components/content-module/code-sample.js
@@ -0,0 +1,13 @@
+/** jsx */
+import {jsx} from 'theme-ui';
+import {preToCodeBlock} from 'mdx-utils';
+import Highlight, {defaultProps} from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/nightOwl';
+
+const CodeSample = (props) => {
+	const codeProps = preToCodeBlock();
+
+	return;
+};
+
+export default CodeSample;

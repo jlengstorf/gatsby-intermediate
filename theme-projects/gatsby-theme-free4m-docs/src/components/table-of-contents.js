485c678319a1cbce779490f60bd0beb53df498b6
David Pineda <david.pineda@deparadise.com>
2020-04-11 10:49:54 -0700

    first pass on TOC component

--- theme-projects/gatsby-theme-free4m-docs/src/components/table-of-contents.js
+++ theme-projects/gatsby-theme-free4m-docs/src/components/table-of-contents.js
@@ -2,6 +2,45 @@
 import {jsx} from 'theme-ui';
 import {graphql, Link, useStaticQuery} from 'gatsby';
 
-const TableOfContents = () => {};
+const TableOfContents = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			allDocsPage {
+				nodes {
+					id
+					title
+					path
+				}
+			}
+		}
+	`);
+
+	const pages = data.allDocsPage.nodes;
+
+	return (
+		<div>
+			<h2></h2>
+			<ul>
+				{pages.map((id, path, title) => {
+					<li key={id}>
+						<Link
+							to={path}
+							sx={{
+								'&.active': {
+									fontStyle: 'italic',
+									textDecoration: 'none',
+									'::after': {content: '" (currently viewing)"'},
+								},
+							}}
+							activeClassName="active"
+						>
+							{title}
+						</Link>
+					</li>;
+				})}
+			</ul>
+		</div>
+	);
+};
 
 export default TableOfContents;

eeb2acb54334f10f5f2c18cb8c3b3f048084aaee
David Pineda <david.pineda@deparadise.com>
2020-04-11 10:24:25 -0700

    Setup jsx on document-page

--- theme-projects/gatsby-theme-free4m-docs/src/components/document-page.js
+++ theme-projects/gatsby-theme-free4m-docs/src/components/document-page.js
@@ -1,6 +1,7 @@
+/** @jsx jsx **/
 // 2.) >> Renders provided MDX content ontop of the ThemeLayout >>
 
-import React from 'react';
+import {jsx} from 'theme-ui';
 import {MDXRenderer} from 'gatsby-plugin-mdx';
 
 import ThemeLayout from './theme-layout';
@@ -9,7 +10,17 @@ const DocumentPage = ({page}) => (
 	<ThemeLayout>
 		<h1>{page.title}</h1>
 		<MDXRenderer>{page.body}</MDXRenderer>
-		<p>This page was updated: {page.updated}.</p>
+		<p
+			sx={{
+				borderTop: (theme) => `1px solid ${theme.colors.muted}`,
+				color: 'muted',
+				fontSize: 14,
+				mt: 2,
+				pt: 2,
+			}}
+		>
+			This page was updated: {page.updated}.
+		</p>
 	</ThemeLayout>
 );
 

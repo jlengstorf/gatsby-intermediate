7b952aba632355dd6ef3e026891805396911595c
David Pineda <david.pineda@deparadise.com>
2020-04-11 18:58:40 -0700

    Update to CodeSample control flow for A.) simple text B.) Live Provider editing C.) Code Syntax highlight only

--- theme-projects/gatsby-theme-free4m-docs/src/components/content-module/code-sample.js
+++ theme-projects/gatsby-theme-free4m-docs/src/components/content-module/code-sample.js
@@ -4,16 +4,39 @@ import {preToCodeBlock} from 'mdx-utils';
 import Highlight, {defaultProps} from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/dracula'; // 2.) /shadesOfPurple 3.) /nightOwl
 
+import {LiveProvider, LiveEditor, LiveError, LivePreview} from 'react-live';
+import scope from '../../-editing-scope';
+
 const CodeSample = (props) => {
 	const codeProps = preToCodeBlock(props); // NOTE: << Allows smart syntax awarness
 
+	// A.) Just a text block
 	if (!codeProps) {
 		return <pre {...props} />;
 	}
 
 	const {codeString, language} = codeProps;
+	const codeSampleIsReactlive = codeProps['react-live'];
 
-	return (
+	return codeSampleIsReactlive ? (
+		// B.) CODE SAMPLE is configured for live edit
+		<LiveProvider code={codeString} scope={scope} theme={theme}>
+			<LiveEditor />
+			<LiveError />
+
+			<LivePreview
+				sx={{
+					border: (theme) => `1px solid ${theme.colors.muted}`,
+					p: 4,
+					'div :first-of-type': {
+						mt: 0,
+					},
+					variant: 'react-live',
+				}}
+			/>
+		</LiveProvider>
+	) : (
+		// C.) PRISM CODE SAMPLE DISPLAY
 		<Highlight
 			{...defaultProps}
 			code={codeString}
